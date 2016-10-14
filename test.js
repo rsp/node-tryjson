30be4d17424ca0dde95a1a892c2277e87e59be4b
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-10-14 17:24:10 +0200

    add tests for strignification with fallback

--- test.js
+++ test.js
@@ -51,8 +51,7 @@ test('parsing invalid json', function (t) {
         '',
         {a: 1},
         JSON
-    ], invalid = {a: 1};
-    invalid.b = invalid;
+    ];
     t.plan(json.length * fallback.length);
     json.forEach(function (s) {
         fallback.forEach(function (f) {
@@ -103,14 +102,42 @@ test('strigifying invalid objects', function (t) {
         null,
         0/0,
         undefined
-    ];
-    t.plan(objs.length);
+    ], fallback = [
+        'no fallback',
+        'undefined fallback',
+        'invalid fallback',
+        0,
+        null,
+        false,
+        undefined,
+        '',
+        {a: 1},
+        JSON
+    ], invalid = {a: 1};
+    invalid.b = invalid;
+    t.plan(objs.length * fallback.length);
     objs[0].a = objs[0];
     objs[1].a = [[[[[[[[[[objs[2]]]]]]]]]]];
     objs[2].a = {a: {a: {a: {a: {a: objs[1]}}}}};
     objs.forEach(function (s, i) {
-        json = tryjson.stringify(s);
-        obj = JSON.parse(json);
-        t.ok(obj === null, 'object ' + i + ' should be null');
+        fallback.forEach(function (f) {
+            if (f === 'no fallback') {
+                json = tryjson.stringify(s);
+                obj = JSON.parse(json);
+                t.ok(obj === null, 'object ' + i + ' should be null');
+            } else if (f === 'undefined fallback') {
+                json = tryjson.stringify(s, undefined);
+                obj = JSON.parse(json);
+                t.ok(obj === null, 'object ' + i + ' should be null with undefined fallback');
+            } else if (f === 'invalid fallback') {
+                json = tryjson.stringify(s, invalid);
+                obj = JSON.parse(json);
+                t.ok(obj === null, 'object ' + i + ' should be null with invalid fallback');
+            } else {
+                json = tryjson.stringify(s, invalid);
+                obj = JSON.parse(json);
+                t.ok(obj === f, 'object ' + i + ' should be equal to fallback');
+            }
+        });
     });
 });
