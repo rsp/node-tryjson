f131ebebe7dab27506248774dd0b9fc3f8472af8
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-10-14 17:10:16 +0200

    add test for invalid fallback

--- test.js
+++ test.js
@@ -44,6 +44,7 @@ test('parsing invalid json', function (t) {
         0/0
     ], fallback = [
         'no fallback',
+        'invalid fallback',
         0,
         null,
         false,
@@ -51,13 +52,17 @@ test('parsing invalid json', function (t) {
         '',
         {a: 1},
         JSON
-    ];
+    ], invalid = {a: 1};
+    invalid.b = invalid;
     t.plan(json.length * fallback.length);
     json.forEach(function (s) {
         fallback.forEach(function (f) {
             if (f === 'no fallback') {
                 obj = tryjson.parse(s);
                 t.ok(obj === undefined, s + ' should be parsed to undefined');
+            } else if (f === 'invalid fallback') {
+                obj = tryjson.parse(s, invalid);
+                t.ok(obj === undefined, s + ' with invalid fallback should be parsed to undefined');
             } else {
                 obj = tryjson.parse(s, f);
                 t.ok(obj === f, s + ' should be parsed to fallback');
